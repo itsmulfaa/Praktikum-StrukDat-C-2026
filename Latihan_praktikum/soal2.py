396b3ce1841b2f4010b7d55ebc61e5ae363f8c36
itsmulfaa <itsmulfa@gmail.com>
2026-02-26 22:22:35 +0700

    Menghapus folder latihan_praktikum dan tugas_praktikum di root

--- Latihan_praktikum/soal2.py
+++
@@ -1,8 +0,0 @@
-barang = ("B001", "Laptop Gaming", 15000000)
-print("Harga awal barang:", barang[2])
-
-#Buata tupple dengan harga baru
-barang = ("B001", "Laptop Gaming", 14000000)
-
-kode, nama, harga = barang
-print("Harga baru:", harga)
