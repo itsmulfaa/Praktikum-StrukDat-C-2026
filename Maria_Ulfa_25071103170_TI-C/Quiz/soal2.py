acfa899ee1301640b19b55ded03536ed13a972a3
itsmulfaa <itsmulfa@gmail.com>
2026-05-06 22:17:31 +0700

    rapihin repo

--- Maria_Ulfa_25071103170_TI-C/Quiz/soal2.py
+++
@@ -1,9 +0,0 @@
-stok_gadget = [
-{'merk': 'Samsung', 'tipe': 'S23', 'harga': 12000000},
-{'merk': 'Oppo', 'tipe': 'Reno 10', 'harga': 6000000},
-{'merk': 'Xiaomi', 'tipe': 'Mi 13', 'harga': 10000000},
-{'merk': 'Iphone', 'tipe': '15 Pro', 'harga': 20000000},
-]
-
-def filter_harga(data, min_harga, max_harga):
-    if filter_harga [max_harga] 
